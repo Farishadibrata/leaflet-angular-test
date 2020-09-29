ed549bd1f27702e1ae32a58a60be05ad96132a1d
Faris Hadibrata <Farisraihanhadibrata@gmail.com>
2020-09-29 21:22:47 +0700

    coord

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -8,15 +8,17 @@ import L from 'leaflet';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-
+  map : any
+  lat : string
+  lng : string
   constructor() { }
 
   ngOnInit(): void {
 
-    const map = L.map('mapid').setView([51.505, -0.09], 13);
+    this.map = new L.map('mapid').setView([51.505, -0.09], 13);
+    let map = this.map
     L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
     const provider = new OpenStreetMapProvider();
-
     const searchControl = new GeoSearchControl({
       provider, // required
       style: 'bar'
@@ -24,9 +26,14 @@ export class MapComponent implements OnInit {
     map.addControl(searchControl);
     let marker = L.marker([51.505, -0.09]).addTo(map);
     map.on('move', function (e) {
-      const coord = map.getCenter()
-      marker.setLatLng(coord)
+      marker.setLatLng(map.getCenter())
     })
+  }
+  getValue() :void{
+    let map = this.map
+    const {lat, lng} = map.getCenter()
+    this.lat = lat
+    this.lng = lng
   }
 
 }
